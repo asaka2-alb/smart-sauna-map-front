27c9c4668cd6b2dd323a15286d9ce6a8eac3c4b4
Taisei Watanabe <taisei_watanabe@albert2005.co.jp>
2022-04-26 10:12:03 +0900

    Remove unncessary libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
     "extends": [
         "plugin:react/recommended",
         "airbnb",
-        "plugin:testing-library/react",
         "plugin:cypress/recommended",
     ],
     "parserOptions": {
