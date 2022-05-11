38c5a0b9d75fdc7036124b6d77266583690c43bd
Taisei Watanabe <63327908+taisei-watanabe@users.noreply.github.com>
2022-05-11 09:36:15 +0900

    Add component test (#10)

--- src/components/QueryForm/QueryForm.js
+++ src/components/QueryForm/QueryForm.js
@@ -6,8 +6,8 @@ function QueryForm({ query, onInput, onSubmit }) {
   return (
     <form onSubmit={onSubmit}>
       <Stack direction="row" spacing={1} margin={2}>
-        <TextField label="keywords" variant="standard" value={query} onChange={onInput} fullWidth />
-        <Button variant="outlined" type="submit">
+        <TextField id="search-textfield" label="keywords" variant="standard" value={query} onChange={onInput} fullWidth />
+        <Button id="search-button" variant="outlined" type="submit">
           Submit
         </Button>
       </Stack>
