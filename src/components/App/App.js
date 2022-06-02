62dd1f3202e7f5b33e83ce59cc2755ba7bae296d
Taisei Watanabe <taisei_watanabe@albert2005.co.jp>
2022-06-02 10:21:20 +0900

    Add feature to appear AlertPopup

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   Grid, ThemeProvider, CssBaseline,
 } from '@mui/material';
+import ErrorAlert from '../Alerts/Alerts';
 
 import theme from '../../theme/theme';
 import './App.css';
@@ -14,6 +15,8 @@ function App() {
   const [center, setCenter] = useState({ lat: 35.683542, lng: 139.703338 });
   const [saunas, setSaunas] = useState([]);
   const [query, setQuery] = useState('');
+  const [openAlert, setOpenAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState('');
 
   const handleFormInput = (event) => {
     // state でも property でもなく event.target.value で値を渡す
@@ -22,8 +25,12 @@ function App() {
 
   const handleFormSubmit = (event) => {
     event.preventDefault(); // デフォルトではページのリロードが行われる。これを防ぐため。
-    updateMapViewCenter(setCenter, query);
+    updateMapViewCenter(setCenter, setOpenAlert, setAlertMessage, query);
     updateSaunas(setSaunas, query, '');
+  };
+
+  const handleAlertClose = () => {
+    setOpenAlert(false);
   };
 
   return (
@@ -45,6 +52,7 @@ function App() {
               onInput={handleFormInput}
               onSubmit={handleFormSubmit}
             />
+            <ErrorAlert open={openAlert} onClose={handleAlertClose} message={alertMessage} />
           </Grid>
         </Grid>
         <Maps center={center} saunas={saunas} />
@@ -53,11 +61,21 @@ function App() {
   );
 }
 
-function updateMapViewCenter(setCenter, query) {
+function updateMapViewCenter(setCenter, setOpenAlert, setAlertMessage, query) {
   const url = 'https://smart-sauna-map-back.herokuapp.com/';
   createPromise(url, { query })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
     .then((location) => {
       setCenter({ lat: location.lat, lng: location.lng });
+    })
+    .catch((error) => {
+      setAlertMessage(error.message);
+      setOpenAlert(true);
     });
 }
 
@@ -65,6 +83,12 @@ function updateSaunas(setSaunas, keyword = 'shinjuku', prefecture = '') {
   // Python のサウナイキタイパースサーバにクエリを投げてサウナ情報を取得する関数.
   const url = 'https://smart-sauna-map-back.herokuapp.com/sauna';
   createPromise(url, { keyword, prefecture })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+      return response.json();
+    })
     .then((saunas) => {
       setSaunas(saunas);
     });
@@ -78,8 +102,7 @@ async function createPromise(url, query) {
   };
 
   return fetch(url, requestOptions)
-    .then((response) => response.json())
-    .catch((error) => { throw new Error(error); });
+    .then((response) => response);
 }
 
 export default App;
