e5dc827fe76dad6391e575acbd4fb210a92ecf16
Taisei Watanabe <taisei_watanabe@albert2005.co.jp>
2022-07-27 09:51:28 +0900

    Rename component from 'Responsive' to 'ResponsiveImage'

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -3,14 +3,12 @@ import styled from 'styled-components';
 import pic from '../../data/img/logo.png';
 
 export default function Logo() {
-  const Responsive = styled.img`
+  const ResponsiveImage = styled.img`
     max-width: 100%;
     height: auto;
     padding: 0;
     margin: 0;
   `;
 
-  return (
-    <Responsive src={pic} alt="logo.png" />
-  );
+  return <ResponsiveImage src={pic} alt="logo.png" />;
 }
